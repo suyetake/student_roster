f55924df249a30c7e27b5c9edefd260f8735680d
Sue Uyetake <sue.uyetake@gmail.com>
2015-11-02 18:30:47 -0700

    coding in progress..
    [#106913914]

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,7 @@
 class Student < ActiveRecord::Base
+  validates :cell_phone, :home_phone, :work_phone, :length => { :maximum => 10 }
+  validates :home_phone, presence: true
+  # validates :home_phone, :presence => true
+  validates :home_phone, :on 
+
 end
