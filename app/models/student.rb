003a17c6043f532aa9139746b2529f8b4b4e2893
Sue Uyetake <sue.uyetake@gmail.com>
2015-11-04 18:08:21 -0700

    now have several working validations..
    :presence   :length   :inclusion in:
    
    [#106913914]

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ActiveRecord::Base
-  validates :cell_phone, :home_phone, :work_phone, :length => { :maximum => 10 }
+  validates :cell_phone, :home_phone, :work_phone, :length => {:maximum => 10}
   validates :home_phone, presence: true
   # validates :home_phone, :presence => true
-  validates :home_phone, :on 
-
+  validates :last_name, inclusion: {in: %w(Jones Smith Doe jones smith doe),
+      message: "%{value} is not Jones, Smith, or Doe."}
 end
